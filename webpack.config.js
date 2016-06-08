8d62d9cfa21262d465bc10d9b9c74f1db42a9416
leandro <olegon.main@gmail.com>
2016-06-08 12:30:43 -0300

    - Atualizada a configuração do webpack; - Otimizações para o ambiente de produção;

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,26 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = { 
-    entry: path.resolve(__dirname, 'src/main.js'),
+    entry: './src/main.js',
+
     output: {
         path: path.resolve(__dirname, 'build'),
-        publicPath: path.resolve(__dirname, 'build'),
         filename: 'bundle.js'
-    },     
+    },
+     
     module: {   
         loaders: [{
             test: /\.jsx?$/,
             loader: 'babel-loader',
-            exclude: /node_modules/,
-            query: {
-                presets: ['es2015', 'react']
-            }
+            exclude: /node_modules/
         }] 
     },
+
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        })
+    ]
 };
